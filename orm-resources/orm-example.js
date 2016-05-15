065e90bc9fb21821d300cbd54373baeb375c80a6
mattgrisanu <mgrisanu@gmail.com>
2016-05-14 17:21:26 -0700

    Start on ORM

--- orm-resources/orm-example.js
+++ orm-resources/orm-example.js
@@ -4,41 +4,67 @@
  */
 
 var Sequelize = require('sequelize');
-var db = new Sequelize('chatter', 'root', '');
+var db = new Sequelize('chatter', 'root', 'billbillbill', {
+  host: 'localhost',
+  dialect: 'mysql',
+  logging: false
+});
 /* TODO this constructor takes the database name, username, then password.
  * Modify the arguments if you need to */
 
 /* first define the data structure by giving property names and datatypes
  * See http://sequelizejs.com for other datatypes you can use besides STRING. */
-var User = db.define('User', {
+var User = db.define('user', {
   username: Sequelize.STRING
 });
 
-var Message = db.define('Message', {
-  userid: Sequelize.INTEGER,
+var Message = db.define('messages', {
+  userId: Sequelize.INTEGER,
   text: Sequelize.STRING,
+  roomId: Sequelize.INTEGER
+});
+
+var Room = db.define('room', {
   roomname: Sequelize.STRING
 });
 
+
+    Room.hasMany(Message);
+    User.hasMany(Message);
+
+    Room.sync();
+    User.sync();
+
+
+
 /* Sequelize comes with built in support for promises
  * making it easy to chain asynchronous operations together */
-User.sync()
-  .then(function() {
-    // Now instantiate an object and save it:
-    return User.create({username: 'Jean Valjean'});
-  })
-  .then(function() {
-    // Retrieve objects from the database:
-    return User.findAll({ where: {username: 'Jean Valjean'} });
-  })
-  .then(function(users) {
-    users.forEach(function(user) {
-      console.log(user.username + ' exists');
-    });
-    db.close();
-  })
-  .catch(function(err) {
-    // Handle any error in the chain
-    console.error(err);
-    db.close();
-  });
+
+
+
+// User.sync()
+//   .then(function() {
+//     Room.sync();
+//   })
+//   .then(function() {
+//     Message.sync();
+//   })
+//   .then(function() {
+//     // Now instantiate an object and save it:
+//     return User.create({username: 'Jean Valjean'});
+//   })
+//   .then(function() {
+//     // Retrieve objects from the database:
+//     return User.findAll({ where: {username: 'Jean Valjean'} });
+//   })
+//   .then(function(users) {
+//     users.forEach(function(user) {
+//       console.log(user.username + ' exists');
+//     });
+//     db.close();
+//   })
+//   .catch(function(err) {
+//     // Handle any error in the chain
+//     console.error(err);
+//     db.close();
+//   });
